dbc97fa4b7e524ea4cf66f877a6f1b6b1977b8c0
Halston <tsengyiyi1031@gmail.com>
2023-02-21 13:27:56 +0800

    Add Files

---
+++ Practice/0221_LINQ_Practice/0221_LINQ_Practice/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0221_LINQ_Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("（奇偶判斷）論人一串帶有逗號的數宇序列: ");
+            string[] input2 = Console.ReadLine().Split(',');
+            var odd = input2.Where(x => int.Parse(x) % 2 != 0);
+            var even = input2.Where(x => int.Parse(x) % 2 == 0);
+            Console.WriteLine($"奇數：{string.Join(",", odd)}");
+            Console.WriteLine($"偶數：{string.Join(",", even)}");
+        }
+    }
+}
---
+++ Practice/0221_LINQ_Practice/Reverse/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("（序列反轉）翰人一串带有逗號的數宇序列：");
+            string[] input = Console.ReadLine().Split(',');
+            var result = input.Reverse();
+            Console.WriteLine(string.Join("", result));
+            Console.Write("\n");
+        }
+    }
+}
